820809c6a4ef48f71afb9cd38eab927b5753d552
Dylan Lu <dylanelu@gmail.com>
2025-03-08 01:18:00 -0800

    starting but very confused

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Attention is All You Need
+
 PyTorch implementation of "Attention is All You Need"
+
+## Notes for me
+
+Pipeline
+
+- Tokenize text. Pass the encoding to model
+- Context length is 1024
+- We need an embedding (seems to be 768 per token)
+- Feed to
+
+Goals:
+
+- Recreate GPT-2 without looking back at the Karpathy videos or github
--- model.py
+++ model.py
@@ -1,9 +1,38 @@
 import torch
+import torch.nn as nn
+import tiktoken
+
+tokenizer = tiktoken.get_encoding("gpt2")
 
 class Transformer:
     def __init__(self):
         pass
 
-class Model:
+class Model(nn.Module):
     def __init__(self):
-        pass
+        super().__init__()
+
+        vocab_size, embedding_dim = 50257, 768 # Based on gpt2 specs
+        num_heads = 6
+        context_size = 1024
+
+        # Token and Positional Embeddings
+        self.tok_emb = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embedding_dim) 
+        self.pos_emb = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embedding_dim) 
+
+        self.attn = nn.MultiheadAttention(embedding_dim, num_heads)
+        self.W = torch.randn()
+        
+        
+    def forward(self, x):
+        hidden = self.tok_emb(x) + self.pos_emb(x) # Token embedding and position embedding. Gradients flow through both, so we learn both
+        attn_output = self.attn.forward()
+
+
+
+
+
+input_tensor = torch.tensor(tokenizer.encode("Dylan's GPT2"))
+model = Model()
+print(model)
+print(model(input_tensor))
