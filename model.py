d1d685bed6deba853c2c49c02f234cad2880506e
Dylan Lu <dylanelu@gmail.com>
2025-03-10 22:53:43 -0700

    progress on attention

--- model.py
+++ model.py
@@ -18,15 +18,18 @@ class Model(nn.Module):
 
         # Token and Positional Embeddings
         self.tok_emb = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embedding_dim) 
-        self.pos_emb = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embedding_dim) 
+        self.pos_emb = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embedding_dim)
 
-        self.attn = nn.MultiheadAttention(embedding_dim, num_heads)
-        self.W = torch.randn()
+        # Attention
+        self.multihead_attn = nn.MultiheadAttention(embed_dim=embedding_dim, num_heads=num_heads)
+        self.attn_mask = torch.triu(torch.ones(size=(context_size, context_size)), diagonal=1).bool() # Triangle; True means don't attend
         
         
     def forward(self, x):
-        hidden = self.tok_emb(x) + self.pos_emb(x) # Token embedding and position embedding. Gradients flow through both, so we learn both
-        attn_output = self.attn.forward()
+        x = self.tok_emb(x) + self.pos_emb(x) # Give some positional info. Gradients flow through both, so we learn both (instead of using sin/cos wave method)
+        attn_output = self.multihead_attn.forward(query=x, key=x, value=x,attn_mask=self.attn_mask)
+        
+
 
 
 
